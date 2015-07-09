569eb0615bd208ee4f2dbf09364c939f848cc336
rkp8000 <rpang.contact@gmail.com>
2015-07-09 15:19:47 -0700

    Clean up a little bit.

--- linear_search/demo/config/demo_rectangular_plumes_solid.py
+++ linear_search/demo/config/demo_rectangular_plumes_solid.py
@@ -1,8 +1,12 @@
-SRC_DENSITY = 0.05  # per m^2
+from __future__ import division
+import numpy as np
+
+SRC_DENSITY = 0.02  # per m^2
 SEARCH_TIME_MAX = 20  # s
 SPEED = 0.5  # m/s
+THETA = np.pi / 4
 DT = .1
 
 # HIT PROBABILITY PARAMS
-PARAMS = {'dim_x': .9,
-          'dim_y': .1}
+PARAMS = {'dim_x': 2,
+          'dim_y': .3}
--- linear_search/demo/demo_rectangular_plumes_solid.py
+++ linear_search/demo/demo_rectangular_plumes_solid.py
@@ -9,16 +9,24 @@ from config.demo_rectangular_plumes_solid import *
 
 
 def main():
-    agent = search_agent.LinearSearcher(theta=np.pi/2, speed=SPEED)
     hit_probability_function = hit_probability_functions.uniform_box
-    sim = simulation.Simulation(agent, hit_probability_function, PARAMS,
-                                SRC_DENSITY, SEARCH_TIME_MAX, DT)
+    sim = simulation.Simulation(hit_probability_function, PARAMS,
+                                SRC_DENSITY, SEARCH_TIME_MAX, DT,
+                                plume_map_resolution=(500, 500))
+
+    agent1 = search_agent.LinearSearcher(theta=THETA, speed=SPEED)
+    sim.agent = agent1
 
     sim.set_src_positions('random')
 
     _, ax = plt.subplots(1, 1)
-    sim.run_with_plot(ax, draw_every=20)
+    sim.run(with_plot=True, ax=ax, draw_every=20)
 
+    agent2 = search_agent.LinearSearcher(theta=(THETA + np.pi), speed=SPEED)
+
+    sim.reset()
+    sim.agent = agent2
+    sim.run(with_plot=True, ax=ax, draw_every=20)
     plt.show(block=True)
 
 

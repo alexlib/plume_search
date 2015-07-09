6897af6538579effed5747d4225ddc2a8d523ba0
rkp8000 <rpang.contact@gmail.com>
2015-07-09 14:17:02 -0700

    Add code to run basic demo with rectangular plume.

---
+++ linear_search/demo/config/demo.py
@@ -0,0 +1,8 @@
+SRC_DENSITY = 0.05  # per m^2
+SEARCH_TIME_MAX = 20  # s
+SPEED = 0.5  # m/s
+DT = .1
+
+# HIT PROBABILITY PARAMS
+PARAMS = {'dim_x': .9,
+          'dim_y': .1}
---
+++ linear_search/demo/demo.py
@@ -0,0 +1,26 @@
+from __future__ import division, print_function
+import numpy as np
+import matplotlib.pyplot as plt
+
+import search_agent
+import hit_probability_functions
+import simulation
+from config.demo import *
+
+
+def main():
+    agent = search_agent.LinearSearcher(theta=np.pi/2, speed=SPEED)
+    hit_probability_function = hit_probability_functions.uniform_box
+    sim = simulation.Simulation(agent, hit_probability_function, PARAMS,
+                                SRC_DENSITY, SEARCH_TIME_MAX, DT)
+
+    sim.set_src_positions('random')
+
+    _, ax = plt.subplots(1, 1)
+    sim.run_with_plot(ax, draw_every=20)
+
+    plt.show(block=True)
+
+
+if __name__ == '__main__':
+    main()

b2c105b839c34c98047b2b85f0ad71e773ad44da
rkp8000 <rpang.contact@gmail.com>
2015-07-09 15:40:41 -0700

    Write code to examine when plume detections are probabilistic.

---
+++ linear_search/figures/config/rectangular_plumes_probabilistic_vary_theta.py
@@ -0,0 +1,19 @@
+from __future__ import division
+import numpy as np
+import hit_probability_functions
+
+SRC_DENSITY = 0.02  # per m^2
+SEARCH_TIME_MAX = 20  # s
+SPEED = 0.5  # m/s
+THETA = np.pi / 4
+DT = .1
+
+# HIT PROBABILITY
+HIT_PROBABILITY_FUNCTION = hit_probability_functions.uniform_box_probabilistic
+PARAMS = {'dim_x': 3,
+          'dim_y': .1,
+          'p': .02}
+
+# LOOPING PARAMETERS
+N_ENVIRONMENTS = 100
+THETAS = np.linspace(-np.pi, np.pi, 33)

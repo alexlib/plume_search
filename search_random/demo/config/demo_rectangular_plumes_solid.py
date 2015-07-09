362bdfe81ca398c2aea536bc695931c0ff053278
rkp8000 <rpang.contact@gmail.com>
2015-07-09 15:51:49 -0700

    Add demos for random walk searcher.

---
+++ search_random/demo/config/demo_rectangular_plumes_solid.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+SRC_DENSITY = 0.02  # per m^2
+SEARCH_TIME_MAX = 20  # s
+SPEED = 0.5  # m/s
+DT = .1
+
+# HIT PROBABILITY PARAMS
+PARAMS = {'dim_x': 2,
+          'dim_y': .3}
---
+++ search_random/demo/demo_rectangular_plumes_solid.py
@@ -0,0 +1,33 @@
+from __future__ import division, print_function
+import matplotlib.pyplot as plt
+
+import search_agent
+import hit_probability_functions
+import simulation
+from config.demo_rectangular_plumes_solid import *
+
+
+def main():
+    hit_probability_function = hit_probability_functions.uniform_box_solid
+    sim = simulation.Simulation(hit_probability_function, PARAMS,
+                                SRC_DENSITY, SEARCH_TIME_MAX, DT,
+                                plume_map_resolution=(500, 500))
+
+    agent1 = search_agent.RandomSearcher(speed=SPEED)
+    sim.agent = agent1
+
+    sim.set_src_positions('random')
+
+    _, ax = plt.subplots(1, 1)
+    sim.run(with_plot=True, ax=ax, draw_every=20)
+
+    agent2 = search_agent.RandomSearcher(speed=SPEED)
+
+    sim.reset()
+    sim.agent = agent2
+    sim.run(with_plot=True, ax=ax, draw_every=20)
+    plt.show(block=True)
+
+
+if __name__ == '__main__':
+    main()

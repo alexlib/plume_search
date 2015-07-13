c7cdc7b2055be29a9ae1659b9c990e8773b0fdf5
rkp8000 <rpang.contact@gmail.com>
2015-07-13 12:10:40 -0700

    Change some params.

--- search_linear/figures/config/rectangular_plumes_probabilistic_vary_theta.py
+++ search_linear/figures/config/rectangular_plumes_probabilistic_vary_theta.py
@@ -12,8 +12,8 @@ DT = .1
 HIT_PROBABILITY_FUNCTION = hit_probability_functions.uniform_box_probabilistic
 PARAMS = {'dim_x': 3,
           'dim_y': .1,
-          'p': .02}
+          'p': .05}
 
 # LOOPING PARAMETERS
-N_ENVIRONMENTS = 100
+N_ENVIRONMENTS = 1000
 THETAS = np.linspace(-np.pi, np.pi, 33)

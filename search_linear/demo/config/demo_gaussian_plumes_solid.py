639b5821fcfea706bab6a4c4f47d3ec6a559c393
rkp8000 <rpang.contact@gmail.com>
2015-07-15 13:55:31 -0700

    Settle down on decent parameter set for demo.

--- search_linear/demo/config/demo_gaussian_plumes_solid.py
+++ search_linear/demo/config/demo_gaussian_plumes_solid.py
@@ -1,14 +1,14 @@
 from __future__ import division
 import numpy as np
 
-SRC_DENSITY = 0.02  # per m^2
+SRC_DENSITY = 0.01  # per m^2
 SEARCH_TIME_MAX = 20  # s
 SPEED = 0.5  # m/s
 THETA = np.pi / 4
 DT = .1
 
 # HIT PROBABILITY PARAMS
-PARAMS = {'r': .01,
+PARAMS = {'r': .002,
           'd': 0.02,
           'w': 0.5,
-          'th': .008}
+          'th': .001}
